740ec33569dc833bce5b5432e14b9d027f70b394
Robert Ji <jicheng321@yahoo.com.cn>
2012-12-12 15:50:34 +0800

    Initially establish the algo repository

---
+++ src/Z1001.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+// public class Main
+public class Z1001 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNextInt()) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			System.out.println(a + b);
+		}
+	}
+}
+
+
